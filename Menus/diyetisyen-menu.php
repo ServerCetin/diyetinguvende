6cc9a10e96db6bf2cea600e9e2fc8c7dd41feaf1
gzdectnkya <gozdecetinkaya2@gmail.com>
2019-12-29 06:05:31 +0300

    menu

--- Menus/diyetisyen-menu.php
+++ Menus/diyetisyen-menu.php
@@ -9,28 +9,17 @@ echo '
 
     <header>
         <h1><a href="#">Diyetin Güvende!</a></h1>
-        <br>
 
-        <div class ="kullanicibil">
-            <h3> Kullanıcı Adı: <a href="../ortak/kullaniciprofili.php">'.$username.'</a> </h3>
-
-            <h3>Ad: <label>'.$name.'</label></h3>
+<br>
     </header>
-
-
     <nav id="mainnav">
         <ul>
-            </div>
-            <li class="selected-item" style="background-color:forestgreen"><a href="diyetisyen.php"> Hastalarım</a></li>
-            <li><a href="yenidiyetlistesi.php">Yeni Diyet Listesi Oluştur</a></li>
-            <li><a href="hastakaydet.php">Hasta Kaydet</a></li>
-            <li><a href="diyetisyenmesajlari.php">Mesajlarim</a></li>
-            <li ><a href="destek.php">Destek</a></li>
-            <li ><a href="../cikis-yap.php">Çıkış Yap</a></li>
+          
+           
             <li><font face="Oxygen" ><a href="../Ortak/kullanici-profili.php">'.$name.' '.$soyad.' <br>@'.$username.'</font></a></li>
             <li class="selected-item" style="background-color:forestgreen"><a href="/Diyetisyen/diyetisyen.php"> Hastalarım</a></li>
-            <li><a href="/Diyetisyen/yeni-diyet-listesi.php">Yeni Diyet Listesi Oluştur</a></li>
-            <li><a href="/Diyetisyen/hasta-kaydet.php">Hasta Kaydet</a></li>
+            <li><a href="/Diyetisyen/yeni-diyet-listesi.php">Diyet Listesi Ekle</a></li>
+            <li><a href="/Diyetisyen/hasta-kaydet.php">Liste Güncelle</a></li>
             <li><a href="/Diyetisyen/diyetisyen-mesajlari.php">Mesajlarim</a></li>
             <li ><a href="../Ortak/destek.php">Destek</a></li>
             <li ><a href="../logout.php">Çıkış Yap</a></li>

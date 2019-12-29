1ba5e8987f9fdc55f2f184326eb64541c2f054c7
Server ÇETİN <servercetin@hotmail.com>
2019-12-29 07:45:46 +0300

    .

--- Diyetisyen/diyetisyen.php
+++ Diyetisyen/diyetisyen.php
@@ -25,13 +25,10 @@ $username = $_SESSION["username"];
 	    <article>
 			
 			<div class="beyaz" style="padding-top: 50px"  >
-			<form action="#" method="get">		
+			<form action="#" method="POST">
 			<fieldset>
 				 <legend>Hasta Kaydet ve Liste Gönder</legend><br><br>
-			
-			
-				
-				
+
 				<p>Hastanın Kullanıcı Adı:
 						<input type="text" name="username" id="username" value="" /><br /></p>
 						
@@ -40,18 +37,18 @@ $username = $_SESSION["username"];
                     <?php
                     include '../baglan.php';
                     $id = $_SESSION["Id"];
-                    $listele = $db->query("SELECT * FROM diyettablosu where Di", PDO::FETCH_ASSOC);
+                    $listele = $db->query("SELECT * FROM diyettablosu where DiyetisyenId =$id", PDO::FETCH_ASSOC);
                     if ( $listele->rowCount() ) //rawcountu 0 değilse
                     {
                         foreach( $listele as $gelenveri )
                         {
-                            echo '<option value"'.$gelenveri['Id'].'">'.$gelenveri['TabloAdi'].'</option>';
+                            echo '<option value="'.$gelenveri['Id'].'">'.$gelenveri['TabloAdi'].'</option>';
                         }
                     }
                     ?>
                     </select>
 				<br>
-			<p><a href="#" class="button buttonS" style="margin-left:80%;"name="kodgonder">Gönder</a>
+			<p><input type="submit" style="margin-left:80%;"name="kullaniciEkle" value="Ekle">
 			<br><br>
 			<h4>Hastalarım</h4><br>
 				<table id="hastalistesi">
@@ -60,60 +57,31 @@ $username = $_SESSION["username"];
 	<th style="width:200px;">Git</th>
   </tr>
              <?php
-             /*
-             include 'baglan.php';
-             $listele = $db->query("SELECT * FROM kullanici where KullaniciTurId=3", PDO::FETCH_ASSOC);
-             if ( $listele->rowCount() ) //rawcountu 0 değilse
+
+             include '../baglan.php';
+             $id = $_SESSION['Id'];
+             $listele = $db->query("SELECT * FROM kullanici INNER JOIN hastabilgi ON kullanici.Id=KullaniciId where DiyetisyenId='$id'", PDO::FETCH_ASSOC);
+             if ($listele->rowCount()) //rawcountu 0 değilse
              {
                  foreach( $listele as $gelenveri )
                  {
-                     echo "Id'si: ".$gelenveri['Id']." Adı: ".$gelenveri['Ad']. "<br />";
+                     echo '<tr>
+                            <td>'.$gelenveri['Ad'].' '.$gelenveri['Soyad'].'</td>
+                            <td><a href="diyetisyenhastalari.html" class="button button-reversed">Git</a>
+                               <form method="post"> <input type="hidden" name="kullaniciIds" value="'.$gelenveri['KullaniciId'].'">
+                               <input type="submit" name="kaldir" value="Kaldır"></form>
+                            </td>
+                          </tr>
+                     ';
                  }
              }
-*/
+
              ?>
 
-  <tr>
-    <td>Gözde Çetinkaya</td>
-    <td><a href="diyetisyenhastalari.html" class="button button-reversed">Git</a>
-		<a href="#" class="button">Kaldır</a></td>
-  </tr>
-  <tr>
-    <td>Semih Hatıl</td>
-   
-    <td><a href="diyetisyenhastalari.html" class="button button-reversed">Git</a>
-		<a href="#" class="button">Kaldır</a></td>
-  </tr>
-  <tr>
-    <td>Server Çetin</td>
-    
-    <td><a href="diyetisyenhastalari.html" class="button button-reversed">Git</a>
-		<a href="#" class="button">Kaldır</a></td>
-  </tr>
-  <tr>
-    <td>Arda Çekem</td>
-    
-    <td><a href="diyetisyenhastalari.html" class="button button-reversed">Git</a>
-		<a href="#" class="button">Kaldır</a></td>
-  </tr>
-  <tr>
-    <td>Tolgahan Acar</td>
-    
-    <td><a href="diyetisyenhastalari.html" class="button button-reversed">Git</a>
-		<a href="#" class="button">Kaldır</a></td>
-  </tr>
-  
 </table>
 			</form>	
 				</div>
 		</article>
-			
-		
-				
-		
-
-			
-			
 
 		</section>
 
@@ -124,3 +92,28 @@ $username = $_SESSION["username"];
 
 </body>
 </html>
+
+<?php
+if(isset($_POST['kullaniciEkle'])){
+
+    $username = $_POST['username'];
+    $id = $_SESSION['Id'];
+    $tabloId = $_POST['diyetList'];
+    include ('../baglan.php');
+
+    $query = $db->query("SELECT * FROM kullanici WHERE KullaniciAdi ='{$username}'")->fetch(PDO::FETCH_ASSOC);
+    if ( $query ){
+        $kId = $query['Id'];
+        $insert = $db -> exec("UPDATE hastabilgi SET DiyetisyenId='$id',DiyetTabloId='$tabloId' where KullaniciId='$kId'");
+    }
+}
+if(isset($_POST['kaldir'])){
+    $uId = $_POST['kullaniciIds'];
+    $query = $db->query("SELECT * FROM hastabilgi WHERE KullaniciId ='{$uId}'")->fetch(PDO::FETCH_ASSOC);
+    if ($query){
+        $insert = $db -> exec("UPDATE hastabilgi SET DiyetisyenId=null, DiyetTabloId=null where KullaniciId='$uId'");
+    }
+}
+
+
+?>

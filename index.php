bf34c087e2a5667572fb2ca1f8d915bfadfd2529
Server ÇETİN <servercetin@hotmail.com>
2020-01-14 12:35:25 +0300

    v1.0
    Sunum öncesi index fix

--- index.php
+++ index.php
@@ -4,13 +4,13 @@ ob_start();
 
 if(isset($_SESSION["kullaniciTur"])) {
     if ($_SESSION["kullaniciTur"] == "Kullanici")
-        header("Location: ../Kullanici/kullanici-sayfasi.php");
+        header("Location: Kullanici/kullanici-sayfasi.php");
     else if ($_SESSION["kullaniciTur"] == "Spor Hocası")
-        header("Location: ../Koc/koc.php");
+        header("Location: Koc/koc.php");
     else if ($_SESSION["kullaniciTur"] == "Diyetisyen")
-        header("Location: ../Diyetisyen/diyetisyen.php");
+        header("Location: Diyetisyen/diyetisyen.php");
     else if ($_SESSION["kullaniciTur"] == "Yönetici")
-        header("Location: ../Yonetici/destek-istekleri.php");
+        header("Location: Yonetici/destek-istekleri.php");
 }
 else
     header("Location: /giris-yap.php");

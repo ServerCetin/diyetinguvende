345171612cdf2ad6aca7cb06d6a5c8b5532a83f9
Server ÇETİN <servercetin@hotmail.com>
2020-05-10 23:13:11 +0300

    .

--- Ortak/get-menu.php
+++
@@ -1,8 +0,0 @@
-<?php
-if($_SESSION["kullaniciTur"] == "Diyetisyen"){include "../menus/diyetisyen-menu.php";}
-else if($_SESSION["kullaniciTur"] == "Spor Hocası"){include "../menus/koc-menu.php";}
-else if($_SESSION["kullaniciTur"] == "Yönetici"){include "../menus/yonetici-menu.php";}
-else {include "../menus/kullanici-menu.php";}
-
-?>
-

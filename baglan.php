e877d969524a209208a00e290e62280daece40b2
Tolgahan <47753083+tolgahanacar@users.noreply.github.com>
2022-08-03 09:14:18 +0300

    Update baglan.php

--- baglan.php
+++ baglan.php
@@ -2,7 +2,6 @@
 try{
     $db = new PDO("mysql:host=localhost;dbname=diyetinguvende;charset=UTF8","root","");
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}catch(PDOException $HataMesaji){
-    echo $HataMesaji->getMessage();
-    die();
+}catch(PDOException $e){
+    die($e->getMessage());
 }
